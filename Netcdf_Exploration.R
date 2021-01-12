2285a239ff667b3b1c5f4aab4f05042b29990bb4
StephanieBrodie <stephaniebrodie@SWC-SBRODIE4-M.local>
2021-01-12 14:28:16 -0800

    updating to monthly

--- Netcdf_Exploration.R
+++ Netcdf_Exploration.R
@@ -17,9 +17,9 @@ library(cmocean)
 
 #-----Create loop to loop through each year------
 
-file_names <- list.files("~/Dropbox/PRC Particle Tracking/Parcels_output_total/Mon_11_-6_Box_36_38_1_4/", full.names = TRUE)
-output <- as.data.frame(matrix(NA,nrow=24,ncol=5))
-colnames(output) <- c("year","prop_30","prop_32","prop_34","prop_36")
+file_names <- list.files("~/Dropbox/PRC Particle Tracking/Parcels_output_total/backwardsfrommontereyreleasedfor1month/", full.names = TRUE)
+output <- as.data.frame(matrix(NA,nrow=12,ncol=5))
+colnames(output) <- c("month","prop_30","prop_32","prop_34","prop_36")
 counter=1
 for (f in 1:length(file_names)){
   
@@ -29,12 +29,12 @@ for (f in 1:length(file_names)){
   # print(nc_file) #geolocation of 171 tracks for 214 days, at the surface.
   
   #grab year from filename 
-  year <- as.numeric(unlist(strsplit(file_names[f],"_"))[14])
-  print(year)
+  month <- as.numeric(unlist(strsplit(file_names[f],"_"))[9])
+  print(month)
   
   #----Extract variables from Netcdf-----
   #According to 'print(file)' above, there are 5 variables: trajectory, time, lat, lon, and depth
-  id <- ncvar_get(nc_file,'trajectory') #we don't really need to extract this because the dimension of 171 indicates 171 particles. 
+  id <- ncvar_get(nc_file,'trajectory') #we don't really need to extract this because the dimension of 45 indicates 45 particles. 
   lat <- ncvar_get(nc_file,'lat') 
   lon <- ncvar_get(nc_file,'lon')
   z <- ncvar_get(nc_file,'z') #we don't need this yet because data is surface data
@@ -76,31 +76,31 @@ for (f in 1:length(file_names)){
   #Animated GGplot
   #CAUTION: make sure you comment this out if you don't need it, otherwise the loop will take MUCH longer as each animation takes a while to render. 
   #Making this as a first step to understand which points are getting stuck on land
-  animate_map <- ggplot(data = df, aes(x=lon,y=lat))+
-    geom_point()+
-    theme_classic() +  labs(y="", x="") +
-    theme(legend.position="right",legend.title = element_blank())+
-    theme( panel.border = element_rect(colour = "black", fill=NA, size=1)) + #makes a box
-    # scale_fill_gradientn(colours = cmocean("matter")(256),limits = c(0, max(dat$abundance))) +
-    annotation_map(map_data("world"), colour = "black", fill="grey50")+
-    coord_quickmap(xlim=c(-134,-115.8),ylim=c(30,48)) +  #Sets aspect ratio
-    scale_x_continuous(expand = c(0, 0)) +  scale_y_continuous(expand = c(0, 0))+
-    transition_time(date)+
-    ease_aes("linear") +
-    labs(title="Backwards Trajectory 171 Particles {frame_time}") #takes some time
-  animated <- gganimate::animate(animate_map,nframes = 214, fps=5, renderer = gifski_renderer(), rewind=TRUE)#renders in
-  anim_save(paste0('AnimatedMap_Trajectory_backwards_',year,'.gif'), animated)
+  # animate_map <- ggplot(data = df, aes(x=lon,y=lat))+
+  #   geom_point()+
+  #   theme_classic() +  labs(y="", x="") +
+  #   theme(legend.position="right",legend.title = element_blank())+
+  #   theme( panel.border = element_rect(colour = "black", fill=NA, size=1)) + #makes a box
+  #   # scale_fill_gradientn(colours = cmocean("matter")(256),limits = c(0, max(dat$abundance))) +
+  #   annotation_map(map_data("world"), colour = "black", fill="grey50")+
+  #   coord_quickmap(xlim=c(-134,-115.8),ylim=c(30,48)) +  #Sets aspect ratio
+  #   scale_x_continuous(expand = c(0, 0)) +  scale_y_continuous(expand = c(0, 0))+
+  #   transition_time(date)+
+  #   ease_aes("linear") +
+  #   labs(title="Backwards Trajectory 171 Particles {frame_time}") #takes some time
+  # animated <- gganimate::animate(animate_map,nframes = 214, fps=5, renderer = gifski_renderer(), rewind=TRUE)#renders in
+  # anim_save(paste0('AnimatedMap_Trajectory_backwards_',year,'.gif'), animated)
   
   #Next step for Steph: add in velocity field data for the animations
   #Also zoom in on an area where particles are stuck and look at the velocity fields (i.e. are they being pushed onshore by currents)
   
   #-----how to query if particles get stuck-----
   #super simple at this stage
-  n_stuck <- as.numeric(summary(duplicated(df[,c(3,4)]))[3]) #number of days particles stay in the same place 
-  print(paste0("Percent of time stuck on land ", (1-(36594-n_stuck)/36594)*100))
+  # n_stuck <- as.numeric(summary(duplicated(df[,c(3,4)]))[3]) #number of days particles stay in the same place 
+  # print(paste0("Percent of time stuck on land ", (1-(36594-n_stuck)/36594)*100))
   # which(duplicated(df[,c(3,4)])) #index of which particles and days they are getting stuck
   
-  #----Time-series: porportion of particles south of X degrees-----
+  #----Time-series: proportion of particles south of X degrees-----
   #Adjust latitudes depending on whether simulations are FORWARDS or BACKWARDS
   
   #Count how many particles ended south of 30 degrees
@@ -112,14 +112,14 @@ for (f in 1:length(file_names)){
   #Count how many particles ended south of 36 degrees
   p_36 <- length(unique(df$trajectory[ df$lat<=36]))
   
-  p_all <- 171 #because we now 171 unique particles
+  p_all <- dim(id)[2] #number of unique particles in netcdf file
   prop_30 <- (p_30/p_all)
   prop_32 <- (p_32/p_all)
   prop_34 <- (p_34/p_all)
   prop_36 <- (p_36/p_all)
   
   #----write out data----
-  output[counter,1] <- year
+  output[counter,1] <- month
   output[counter,2] <- prop_30
   output[counter,3] <- prop_32
   output[counter,4] <- prop_34
@@ -131,13 +131,17 @@ for (f in 1:length(file_names)){
 }
 summary(output)
 
+
 #-----Make Time-series Plots----
 #Basic R code
-plot(output$year,output$prop_30, col="black", type="b", ylim=c(0,1), ylab="Proportion",xlab="Year", main="Proportion of Particles south of X degrees")
-lines(output$year,output$prop_32, col="green", type="b")
-lines(output$year,output$prop_34, col="red", type="b")
-lines(output$year,output$prop_36, col="blue", type="b")
+output <- output[order(output$month),] #month is out of order
+plot(output$month,output$prop_30, col="black", type="b", ylim=c(0,1), ylab="Proportion",xlab="Month", main="Proportion of Particles south of X degrees")
+lines(output$month,output$prop_32, col="green", type="b")
+lines(output$month,output$prop_34, col="red", type="b")
+lines(output$month,output$prop_36, col="blue", type="b")
 legend("topright",legend=c("30 degrees","32 degrees","34 degrees","36 degrees"),col=c("black","green","red","blue"), lty=c(1,1,1))
 
 #-----END------
+
+
 
