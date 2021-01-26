9a47a9aa68b8a6a19facdb6ee4d528bb65f32d16
HeatherWelch <heather.welch@noaa.gov>
2021-01-26 15:28:15 -0800

    nco for subsetting glorys

--- NCO_glorys_subsetting.R
+++ NCO_glorys_subsetting.R
@@ -2,54 +2,48 @@
 #For red crab particle tracking analysis
 #Desired domain: 0 - 50 degrees N; -150 to -100 degrees west
 #File types: (1) surface u & v, temp & salinity; (2) 0-500m u, v, temp & salinity
+#Note: this code uses command line syntax, as indicted by system2()
 
-#Note: this code uses command line syntax, as indictaed by system()
+#To install NCO, and to see a good summary of what it can do go here: https://github.com/elhazen/NCO_ERD
 
-#-----install NCO----
-#Install developer tools
-# system(xcode-select --install)
+#---library----
+library(glue)
 
-#Install homebrew if you don't have it (homebrew website: https://brew.sh/)
-# system(sudo xcode-select -s /Library/Developer/CommandLineTools) #prepares for homebrew
-# system(/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
+#----Subset u,v,s,t for CCS----
+#Loop through global glorys historical netcdf files (using nested loops because of folder structure)
+#Create new netcdfs for CCS domain for variables u, v, salinity, temp
 
-#Install nco
-# system(brew install nco)
-
-#-----Couple of simple NCO commands----
-#STORING HERE
-# Plot metadata
-# ncdump -h file_in.nc
-
-# Print the values for a single variable (e.g., depth) #CAUTION: only run this for certain variables
-# ncdump -v variable_name file_in.nc
-
-# Extract a single variable to a new file
-# ncks -v variable_name file_in.nc file_out.nc # Example: Extract temperature (variable name thetao) from a GLORYS file to a new file (glorys_temp.nc) ncks -v thetao glorys_sample_day.nc glorys_temp.nc
-
-# Extract temperature for a subset region
-# ncks -F -v thetao -d dimension,min,max,stride -d dimension,min,max,stride file_in.nc file_out.nc
-# Example: Extract temperature from GLORYS for northeast Pacific only ncks -F -v thetao -d longitude,-140.0,-110.0,1 -d latitude,25.0,50.0,1 glorys_sample_day.nc ccs_temp.nc
-# Example: Extract surface temperature from GLORYS for northeast Pacific only ncks -F -v thetao -d longitude,-140.0,-110.0,1 -d latitude,25.0,50.0,1 -d depth 1,1,1 glorys_sample_day.nc ccs_sst.nc
-# FYI: Including decimals in the dimensions will extract between the specified latitude and longitudes. If no decimals are used, then the subsetting is done by index rather than the actual lat/lon 
-# FYI: -F specifies that counting starts at 1 instead of 0
-# FYI: Stride is the spacing between records being extracted. To extract everything, stride is 1, if one wanted to extract every fifth latitude, for example, stride would be 5.
-
-#----Now let's try it for some files
-
-# in_dir <- "~/Dropbox/PRC Particle Tracking/Velocity_NetCDF_Subsurface_0-500m/2002/"
-input_files <- list.files("~/Dropbox/PRC Particle Tracking/Velocity_NetCDF_Subsurface_0-500m/2002/", full.names = TRUE)
-out_dir <- '~/Dropbox/PRC Particle Tracking/test_outputfiles/'
-
-for (i in 1:length(input_files)){
-  print(input_files[[i]])
-  system2("ncdump -h", input =input_files[[i]], stdout = "", stderr = "") #-h is header
-  file <- "mercatorglorys12v1_gl12_mean_20021230_R20030101_0-500mDepth.nc"
-  file_out <- "mercatorglorys12v1_gl12_mean_20021230_R20030101_0-500mDepth_REVISED.nc"
-  system2('ncdump', args=c('-h', file))
-  
-  system2('ncks', args=c('-F', '-v', 'thetao', '-d', 'lon,-130.0,-115.0,1',
-                         '-d', 'lat,25.0,40.0,1',file,file_out))
-
+dir <- "/Volumes/Triple_Bottom_Line/Data/GlobalData/CMEMS_MLD_New/Historical/"
+out_dir <- '/Volumes/Triple_Bottom_Line/Steph_working/PRC/velocity_nc_0-500/'
+for (y in 1993:2017){
+  for (m in c("01","02","03","04","05","06","07","08","09","10","11","12")){
+    print(glue("running year {y} and month {m}"))
+    input_files <- list.files(glue('{dir}{y}/{m}'), full.names = T)
+    for (i in 1:length(input_files)){
+      file <- input_files[[i]]
+      fdate <- unlist(strsplit(input_files[[i]],"_"))[8]
+      year <- lubridate::year(as.Date(fdate,"%Y%m%d"))
+      jdate <- format(as.Date(fdate,"%Y%m%d"), "%j")
+      
+      out_dir_year <- glue("{out_dir}{y}")
+      dir.create(out_dir_year)
+      #File_out needs a specific naming terms inc: full date, year, julian date
+      file_out <- glue("{out_dir}{y}/mercatorglorys12v1_gl12_mean_{fdate}_{year}_{jdate}_uvst_surface_ccs.nc")
+      
+      # system2('ncdump', args=c('-h', file)) #check out headers
+      # system2('ncdump', args=c('-v','depth', file)) #check out depth intervals
+      # system2('ncks') #check out switches for kitchen sink command
+      system2('ncks', args=c('-F', '-v', 'vo,uo,so,thetao',
+                             '-d', 'longitude,-150.0,-100.0,1',
+                             '-d', 'latitude,0.0,50.0,1',
+                             '-d', 'depth,1,31,1',
+                             file,file_out))
+    }
+  }
 }
+
+#----Repeat for NRT file structure----
+#TBC
+
+#----END----
 
